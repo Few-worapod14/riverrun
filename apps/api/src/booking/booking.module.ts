9df36846480e0fbaacbc005e06ee004c8d8d6fe0
dukerspace <montolsaklor@gmail.com>
2024-01-16 00:36:43 +0700

    feat(api): Update booking module

--- apps/api/src/booking/booking.module.ts
+++ apps/api/src/booking/booking.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthModule } from '../auth/auth.module'
 import { BookingAdminController } from './controllers/admin.controller'
 import { BookingController } from './controllers/booking.controller'
 import { Booking } from './entities/booking.entity'
 import { BookingService } from './services/booking.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking])],
+  imports: [TypeOrmModule.forFeature([Booking]), AuthModule],
   controllers: [BookingAdminController, BookingController],
   providers: [BookingService],
   exports: [BookingService]
--- apps/api/src/booking/controllers/admin.controller.ts
+++ apps/api/src/booking/controllers/admin.controller.ts
@@ -1,17 +1,103 @@
-import { Controller } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Put,
+  Query,
+  Req,
+  Res,
+  UseGuards
+} from '@nestjs/common'
+import { BookingUpdateDto, IResponseData, IResponsePaginate } from '@riverrun/interface'
+import { Response } from 'express'
+import { AdminGuard } from '../../auth/guards/admin.guard'
+import { IRequestWithUser } from '../../auth/requet.interface'
+import { Booking } from '../entities/booking.entity'
 import { BookingService } from '../services/booking.service'
 
+@UseGuards(AdminGuard)
 @Controller('/admins/bookings')
 export class BookingAdminController {
   constructor(private bookingService: BookingService) {}
 
-  async create() {}
+  @Get()
+  async findAll(
+    @Req() req: IRequestWithUser,
+    @Res() res: Response,
+    @Query('page') page: number,
+    @Query('limit') limit: number
+  ) {
+    try {
+      const currentPage = page || 1
+      const perPage = limit || 10
+      const query = await this.bookingService.findAll(currentPage, perPage)
+      const total = await this.bookingService.count()
+      const response: IResponsePaginate<Booking[]> = {
+        success: true,
+        total: total,
+        currentPage: currentPage,
+        perPage: perPage,
+        data: query
+      }
+      res.status(HttpStatus.OK).json(response)
+    } catch (error) {
+      const msg: IResponseData<string> = {
+        message: error,
+        success: false
+      }
+      throw new HttpException(msg, HttpStatus.BAD_REQUEST)
+    }
+  }
 
-  async findAll() {}
+  @Get(':id')
+  async findByID(
+    @Req() req: IRequestWithUser,
+    @Res() res: Response,
+    @Param('id', ParseIntPipe) id: number
+  ) {
+    try {
+      const userId = req.user.sub
+      const query = await this.bookingService.findByID(id)
+      const response: IResponseData<Booking> = {
+        data: query,
+        success: true
+      }
 
-  async findByID() {}
+      res.status(HttpStatus.OK).json(response)
+    } catch (error) {
+      const msg: IResponseData<string> = {
+        message: error,
+        success: false
+      }
+      throw new HttpException(msg, HttpStatus.BAD_REQUEST)
+    }
+  }
 
-  async update() {}
+  @Put(':id')
+  async update(
+    @Req() req: IRequestWithUser,
+    @Res() res: Response,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: BookingUpdateDto
+  ) {
+    try {
+      const query = await this.bookingService.update(id, body)
+      const response: IResponseData<string> = {
+        message: 'Booking room update successfully',
+        success: true
+      }
 
-  async remove() {}
+      res.status(HttpStatus.OK).json(response)
+    } catch (error) {
+      const msg: IResponseData<string> = {
+        message: error,
+        success: false
+      }
+      throw new HttpException(msg, HttpStatus.BAD_REQUEST)
+    }
+  }
 }
