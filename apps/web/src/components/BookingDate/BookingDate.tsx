e8b63d9ccc43d5a1545388ce68684eadcf15669a
Nitjaree <nitjaree@vekin.co.th>
2024-01-17 19:30:59 +0700

    create booking form

--- apps/web/src/components/BookingDate/BookingDate.tsx
+++ apps/web/src/components/BookingDate/BookingDate.tsx
@@ -1,4 +1,5 @@
 import { DateInput, DateValue } from '@mantine/dates'
+import { Select, Button } from '@mantine/core';
 import { useState } from 'react'
 import dayjs from 'dayjs';
 
@@ -11,14 +12,32 @@ export const BookingDate = () => {
 
   return (
     <>
-      {/* <DateInput onChange={handleBooking} /> */}
-      <DateInput
-      minDate={new Date()}
-      maxDate={dayjs(new Date()).add(1, 'month').toDate()}
-      label="Date input"
-      placeholder="Date input"
-      onChange={handleBooking}
-    />
+
+      <div className='flex justify-between items-center gap-4 bg-white p-4 rounded-2xl'>
+        <p className='text-2xl font-extrabold'>Book Now</p>
+        <DateInput
+          minDate={new Date()}
+          maxDate={dayjs(new Date()).add(1, 'month').toDate()}
+          label="Check In"
+          placeholder="mm/dd/yyyy"
+          onChange={handleBooking}
+        />
+        <DateInput
+          minDate={new Date()}
+          maxDate={dayjs(new Date()).add(1, 'month').toDate()}
+          label="Check Out"
+          placeholder="mm/dd/yyyy"
+          onChange={handleBooking}
+        />
+        <Select
+          label="Guests"
+          placeholder="1"
+          data={['1', '2', '3', '4']}
+          defaultValue="React"
+          className='w-[56px]'
+        />
+        <Button variant="filled" color="teal" size="xl" >BOOK NOW</Button>
+      </div>
     </>
   )
 }
