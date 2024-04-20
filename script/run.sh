6553fe0f3c14a9fafd01f2353fb5b71587537dca
dukerspace <montolsaklor@gmail.com>
2024-04-20 20:52:58 +0700

    fix: run script copy env

--- script/run.sh
+++ script/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp ../env-dev ../apps/api/.env
-cp ../env-dev ../apps/web/.env
-cd .. && pnpm install && pnpm run build
+cp ../.env-dev ../apps/api/.env
+cp ../.env-dev ../apps/web/.env
+pnpm install && pnpm run build
 pm2 start npm --name "api" -- run start:prod --cwd ../apps/api
 pm2 start npm --name "ui" -- run preview --cwd ../apps/web
