3868e0afe47d1b06e1db1923bb991a828add840f
dukerspace <montolsaklor@gmail.com>
2024-04-22 02:29:22 +0700

    feat: add pm2 config

--- run.sh
+++ run.sh
@@ -3,5 +3,4 @@ docker compose up -d
 cp .env-dev ./apps/api/.env
 cp .env-dev ./apps/web/.env
 pnpm install && pnpm run build
-pm2 start npm --name "api" -- run start:prod --cwd /var/www/riverrun/apps/api
-pm2 start npm --name "ui" -- run preview --cwd /var/www/riverrun/web
+pm2 start ecosystem.config.js
